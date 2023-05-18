ccafc87b2530d3db6be39c0cf87ff85a938a99e8
K Mahesh <mahesh0352002@gmail.com>
2023-05-18 23:33:32 +0530

    final commit

--- sql_connectivity.py
+++ sql_connectivity.py
@@ -1,3 +1,4 @@
+from tabulate import tabulate  #to print data neatly in a table
 import mysql.connector
 con= mysql.connector.connect(host="localhost",user="root",password="123456",database="python_db3")
 
@@ -7,13 +8,35 @@ else:
     print("connection error")
 
 def insert(name,age,city):
-    pass
-def update(name,age,city):
-    pass
+    res=con.cursor()
+    sql="insert into student (name,age,city) values (%s,%s,%s)"
+    stud=(name,age,city)
+    res.execute(sql,stud)
+    con.commit()
+    print("data inserted")
+
+def update(name,age,city,id):
+    res = con.cursor()
+    sql = "update student set name=%s,age=%s,city=%s where id=%s"
+    stud = (name, age, city, id)
+    res.execute(sql,stud)
+    con.commit()
+    print("data updated")
+
 def select():
-    pass
+    res=con.cursor()  #cursor is used  to connect python and mysql
+    sql="select ID,NAME,AGE,CITY from student "
+    res.execute(sql)
+    result=res.fetchall()
+    print(tabulate(result,headers=["ID","NAME","AGE","CITY"]))
+
 def delete(id):
-    pass
+    res = con.cursor()
+    sql = "delete from student where id=%s"
+    stud = (id,) #tuple
+    res.execute(sql,stud)
+    con.commit()
+    print("data delete")
 
 while True:
     print("1.insert data")
@@ -28,12 +51,18 @@ while True:
         city=input("enter the city:")
         insert(name,age,city)
     elif choice==2:
+        id=int(input("enter id:"))
         name=input("enter the name")
         age = int(input("enter the age:"))
         city = input("enter the city:")
-        update(name, age, city)
+        update(name, age, city,id)
     elif choice==3:
         select()
     elif choice==4:
         id=int(input("enter the ID to delete"))
         delete(id)
+    elif choice==5:
+        quit()
+    else:
+        print("invalid selection")
+
