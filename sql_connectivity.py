cda8091dc3ab2310b0f01f4fa77c5e3999574163
K Mahesh <mahesh0352002@gmail.com>
2023-05-18 22:14:04 +0530

    commit

--- sql_connectivity.py
+++ sql_connectivity.py
@@ -5,3 +5,35 @@ if con:
     print("connected")
 else:
     print("connection error")
+
+def insert(name,age,city):
+    pass
+def update(name,age,city):
+    pass
+def select():
+    pass
+def delete(id):
+    pass
+
+while True:
+    print("1.insert data")
+    print("2.Update data")
+    print("3.select data")
+    print("4.Delete data")
+    print("5.exit")
+    choice=int(input("enter your choice:"))
+    if choice==1:
+        name=input("enter the name:")
+        age=int(input("enter the age:"))
+        city=input("enter the city:")
+        insert(name,age,city)
+    elif choice==2:
+        name=input("enter the name")
+        age = int(input("enter the age:"))
+        city = input("enter the city:")
+        update(name, age, city)
+    elif choice==3:
+        select()
+    elif choice==4:
+        id=int(input("enter the ID to delete"))
+        delete(id)
